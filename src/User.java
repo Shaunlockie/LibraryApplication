fa44e62bdd08f66570790bbac2173fa50e241912
Shaun Lockie <44776669+Shaunlockie@users.noreply.github.com>
2021-04-11 10:32:56 +0100

    created ID field to align with auto incrementing id in database

--- src/User.java
+++ src/User.java
@@ -1,7 +1,11 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class User {
+    private int id;
     private String username;
+    private String email;
+    private LocalDate creationDate;
     public Collection collection;
 
     // create a new user
@@ -10,6 +14,12 @@ public class User {
         Scanner in = new Scanner(System.in);
         this.username = in.nextLine();
         System.out.println("Username: " + username);
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
     }
     public void setUsername(String username){
         this.username = username;
