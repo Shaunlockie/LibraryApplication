9c8c52955b2f9644a178fca963a110c0eeb7d119
Shaun Lockie <44776669+Shaunlockie@users.noreply.github.com>
2021-04-13 15:26:37 +0100

    Creating a collection now stores the user id and the collection information to a database along with a serialised object

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,56 +1,49 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.sql.*;
 import java.sql.DriverManager;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LibraryApp {
     public static boolean isRunning;
+    public static boolean loggedIn;
     private static final Scanner in = new Scanner(System.in);
     private static final ArrayList<User> users = new ArrayList<>();
     private static final ArrayList<String> collections = new ArrayList<>();
-    //private static User selectedUser = null;
-    //private static boolean userSet;
-    User u = null;
+    private final List<Collection> userCollections = new ArrayList<Collection>();
     private Statement sqlStatement;
 
     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarydatabase", "root","password");
 
     public LibraryApp() throws SQLException, ClassNotFoundException {
         isRunning = true;
+        loggedIn = false;
         Class.forName("com.mysql.cj.jdbc.Driver");
         this.sqlStatement = con.createStatement();
     }
-    public void programLoop() throws SQLException {
-        while (isRunning) {
-            mainMenu();
-            // pick user
-            int input = readInt("->", 4);
-            if (input == 1) {
-                createUser();
+    public void programLoop(User u) throws SQLException {
+        while (isRunning && !loggedIn) {
+            mainMenu(u);
             }
-            if (input == 2) {
-                loginUser();
-            }
-            if (input == 3) {
-                viewUsers();
-            }
-            if (input == 4) {
-                System.exit(1);
-            }
+        while(isRunning && loggedIn) {
+            userMenu(u);
         }
     }
 
     private User loginUser() throws SQLException {
         System.out.println("Input Username");
         String userAccountName = in.next();
-        try {
+        User u = null;
 
+        try {
             PreparedStatement prepStatement = con.prepareStatement("SELECT * FROM useraccounts WHERE username = (?)");
             prepStatement.setString(1, userAccountName);
             ResultSet r1 = prepStatement.executeQuery();
             String usernameCounter;
-
             while (r1.next()) {
                 u = new User();
                 u.setId(r1.getInt("userId"));
@@ -61,12 +54,12 @@ public class LibraryApp {
                 //System.out.println(u.getId() + " " + u.getUsername()+ " " + u.getEmail()+ " " +  u.getCreatedDate());
                 System.out.println("Welcome: " + u.getUsername());
             }
-            System.out.println("This is running");
-            userMenu();
+            loggedIn = true;
+            userMenu(u);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-       return u;
+        return u;
     }
 
     public void createUser() throws SQLException{
@@ -91,52 +84,68 @@ public class LibraryApp {
         while(result.next()) {
             System.out.println(result.getString("username"));
         }
-        programLoop();
         }
 
-    public void mainMenu(){
+    public void mainMenu(User u) throws SQLException {
         clearConsole();
         System.out.println("What would you like to do?: ");
         System.out.println("1. Create a new user");
         System.out.println("2. Log in to current user");
         System.out.println("3. View all users");
         System.out.println("4.Exit program!");
-    }
+        int input = readInt("->", 4);
+        if (input == 1) {
+            createUser();
+        }
+        if (input == 2) {
+            loginUser();
+        }
+        if (input == 3) {
+            viewUsers();
+        }
+        if (input == 4) {
+            System.exit(1);
+            }
+        }
 
-    public void userMenu() throws SQLException {
+
+    public void userMenu(User u) throws SQLException {
         System.out.println("What would you like to do?: ");
         System.out.println("1. Create a new collection");
         System.out.println("2. View collections");
         System.out.println("3. Settings");
         System.out.println("4. logout");
         int input = readInt("->", 4);
-        if (input == 1){
-            Collection col = new Collection();
-            col.createCollection();
-            userMenu();
-            col.showCollectionInfo();
-        }if (input == 2){
-            userMenu();
-        }if (input == 3){
-            settingsMenu();
+        if (input == 1) {
+            createCollection(u);
         }
-        if (input == 4){
-            LogoutUser();
-            userMenu();
+        if (input == 2) {
+            for (Collection col : userCollections) {
+                System.out.println("Collection: " + col);
+            }
         }
+        if (input == 3) {
+            settingsMenu(u);
+        }
+        if (input == 4) {
+            LogoutUser(u);
+            programLoop(u);
+
+        }
+
     }
-    public void settingsMenu() throws SQLException {
+    public void settingsMenu(User u) throws SQLException {
         System.out.println("Settings:");
-        System.out.println("1. Change Username");
+        System.out.println("2. Change Username");
         System.out.println("2. Change Email");
         System.out.println("3. Go back");
         int input = readInt("->", 4);
         if (input == 1){
-            changeUsername();
+            changeUsername(u);
         }if (input == 2){
-            changeEmail();
+            changeEmail(u);
         }if(input == 3){
-            userMenu();
+            userMenu(u);
         }
     }
 
@@ -160,14 +169,13 @@ public class LibraryApp {
         }
     }
 
-    public void LogoutUser() throws SQLException {
-        userSet = false;
-        selectedUser = null;
+    public void LogoutUser(User u) throws SQLException {
+        u = null;
+        loggedIn = false;
         System.out.println("You are now logged out");
-        programLoop();
     }
 
-    public void changeUsername() throws SQLException {
+    public void changeUsername(User u) throws SQLException {
         System.out.println("Choose a new username: ");
         String newUsername = in.next();
         int updateId = u.getId();
@@ -176,9 +184,9 @@ public class LibraryApp {
         prepStatement.setInt(2, updateId);
         prepStatement.executeUpdate();
         System.out.println("Username changed!  ");
-        userMenu();
+        userMenu(u);
     }
-    private void changeEmail() throws SQLException {
+    private void changeEmail(User u) throws SQLException {
         System.out.println("Choose a new email: ");
         String newEmail = in.next();
         int updateId = u.getId();
@@ -187,13 +195,46 @@ public class LibraryApp {
         prepStatement.setInt(2, updateId);
         prepStatement.executeUpdate();
         System.out.println("Email changed!  ");
-        userMenu();
+        userMenu(u);
     }
-
-    public void createCollection(){
+    public void createCollection(User u) throws SQLException {
+        Collection col = new Collection();
+        col.createCollection();
+        col.showCollectionInfo();
+        userCollections.add(col);
+        serializeCollection(u,col);
 
     }
+    public void serializeCollection(User u, Collection col){
+        byte[] data = null;
+        int userId = u.getId();
+        String collName = col.getName();
+        String collDesc = col.getDesc();
+        LocalDate collCreatedDate = LocalDate.now();
+        try{
+            ByteArrayOutputStream byteColl = new ByteArrayOutputStream();
+            ObjectOutputStream objColl = new ObjectOutputStream(byteColl);
+            objColl.flush();
+            objColl.close();
+            byteColl.close();
+            data = byteColl.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try{
+            PreparedStatement prepStatement = con.prepareStatement("insert into bookcollections (userId,collectionName,collectionDesc,collectionCreatedDate,collectionSerialize)" + "values (?,?,?,?,?)");
+            prepStatement.setInt(1, userId);
+            prepStatement.setString(2, collName);
+            prepStatement.setString(3,collDesc);
+            prepStatement.setDate(4, Date.valueOf(collCreatedDate));
+            prepStatement.setObject(5, data);
+            prepStatement.executeUpdate();
+            System.out.println("THis is running");
 
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
     public ResultSet makeQuery(String givenStatement) throws SQLException {
         setSqlStatement(givenStatement);
         return sqlStatement.executeQuery(givenStatement);
