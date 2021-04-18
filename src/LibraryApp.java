2996c3f9e709086be607fc4662dbc85127fbc00e
Shaun Lockie <44776669+Shaunlockie@users.noreply.github.com>
2021-04-18 21:58:58 +0100

    created interaction table which stores collection id, user id and book id.

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,6 +1,4 @@
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.sql.*;
 import java.sql.DriverManager;
 import java.time.LocalDate;
@@ -12,9 +10,7 @@ public class LibraryApp {
     public static boolean isRunning;
     public static boolean loggedIn;
     private static final Scanner in = new Scanner(System.in);
-    private static final ArrayList<User> users = new ArrayList<>();
-    private static final ArrayList<String> collections = new ArrayList<>();
-    private final List<Collection> userCollections = new ArrayList<Collection>();
+    //private final List<Collection> userCollections = new ArrayList<Collection>();
     private Statement sqlStatement;
 
     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarydatabase", "root","password");
@@ -25,7 +21,7 @@ public class LibraryApp {
         Class.forName("com.mysql.cj.jdbc.Driver");
         this.sqlStatement = con.createStatement();
     }
-    public void programLoop(User u) throws SQLException {
+    public void programLoop(User u) throws SQLException, IOException, ClassNotFoundException {
         while (isRunning && !loggedIn) {
             mainMenu(u);
             }
@@ -35,48 +31,39 @@ public class LibraryApp {
     }
 
     private User loginUser() throws SQLException {
+        User u = null;
         System.out.println("Input Username");
         String userAccountName = in.next();
-        User u = null;
 
         try {
             PreparedStatement prepStatement = con.prepareStatement("SELECT * FROM useraccounts WHERE username = (?)");
             prepStatement.setString(1, userAccountName);
             ResultSet r1 = prepStatement.executeQuery();
-            String usernameCounter;
-            while (r1.next()) {
+            if (r1.next()) {
                 u = new User();
-                u.setId(r1.getInt("userId"));
+                u.setID(r1.getInt("userID"));
                 u.setUsername(r1.getString("username"));
                 u.setEmail(r1.getString("email"));
                 u.setCreatedDate(r1.getDate("createdDate").toLocalDate());
-
-                //System.out.println(u.getId() + " " + u.getUsername()+ " " + u.getEmail()+ " " +  u.getCreatedDate());
                 System.out.println("Welcome: " + u.getUsername());
+                loggedIn = true;
+                userMenu(u);
             }
-            loggedIn = true;
-            userMenu(u);
+
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
         return u;
     }
 
-    public void createUser() throws SQLException{
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please input a username");
-        String userAccountName = in.nextLine();
-
-        System.out.println("Please input a Email");
-        String userEmail = in.nextLine();
-
-        LocalDate userDateCreated = LocalDate.now();
+    public User createUser() throws SQLException{
+        User u = new User();
         PreparedStatement prepStatement = con.prepareStatement("insert into userAccounts (username,email,createdDate)" + "values (?,?,?)");
-        prepStatement.setString(1,userAccountName);
-        prepStatement.setString(2,userEmail);
-        prepStatement.setDate(3, Date.valueOf(userDateCreated));
-
+        prepStatement.setString(1,u.getUsername());
+        prepStatement.setString(2,u.getEmail());
+        prepStatement.setDate(3, Date.valueOf(u.getCreatedDate()));
         prepStatement.executeUpdate();
+        return u;
     }
 
     public void viewUsers() throws SQLException {
@@ -109,32 +96,106 @@ public class LibraryApp {
         }
 
 
-    public void userMenu(User u) throws SQLException {
+    public void userMenu(User u) throws SQLException, IOException, ClassNotFoundException {
         System.out.println("What would you like to do?: ");
         System.out.println("1. Create a new collection");
-        System.out.println("2. View collections");
+        System.out.println("2. View all collections - Currently only adding");
         System.out.println("3. Settings");
         System.out.println("4. logout");
         int input = readInt("->", 4);
         if (input == 1) {
-            createCollection(u);
+            newCollection(u);
         }
         if (input == 2) {
-            for (Collection col : userCollections) {
-                System.out.println("Collection: " + col);
-            }
+            //retrieveCollections(u);
+            loadCollection(u);
         }
         if (input == 3) {
             settingsMenu(u);
         }
         if (input == 4) {
-            LogoutUser(u);
+            LogoutUser();
             programLoop(u);
-
         }
 
     }
-    public void settingsMenu(User u) throws SQLException {
+    public Collection newCollection(User u) throws SQLException, IOException, ClassNotFoundException {
+        Collection col = new Collection();
+        col.createCollection();
+        col.showCollectionInfo();
+        //userCollections.add(col);
+        PreparedStatement prepStatement = con.prepareStatement("insert into collections (colName,colDesc,colCreatedDate,userID)" + "values (?,?,?,?)");
+        prepStatement.setString(1,col.getName());
+        prepStatement.setString(2, col.getDesc());
+        prepStatement.setDate(3, Date.valueOf(col.getCreatedDate()));
+        prepStatement.setInt(4, u.getID());
+        prepStatement.executeUpdate();
+        return col;
+    }
+    public Collection loadCollection(User u) throws SQLException, IOException, ClassNotFoundException {
+        Collection col = null;
+        System.out.println("Which collection would you like ot view");
+        String colPick = in.next();
+        try {
+            PreparedStatement prepStatement = con.prepareStatement("SELECT * FROM collections WHERE (userID, colName) = (?,?)");
+            prepStatement.setInt(1, u.getID());
+            prepStatement.setString(2, colPick);
+            ResultSet r3 = prepStatement.executeQuery();
+            if(r3.next()) {
+                col = new Collection();
+                col.setCollectionID(r3.getInt("colID"));
+                col.setName(r3.getString("colName"));
+                col.setDesc(r3.getString("colDesc"));
+                col.setCreatedDate(r3.getDate("colCreatedDate").toLocalDate());
+                col.setUserID(r3.getInt("userID"));
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }finally{
+            addBookToCollection(u,col);
+        }
+        return col;
+    }
+
+    private void addBookToCollection(User u, Collection col) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Enter book title or press 1 to exit");
+        Book b = null;
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String bookTitle = in.nextLine();
+            try {
+                PreparedStatement prepStatement = con.prepareStatement("SELECT * FROM books WHERE bookTitle = (?)");
+                prepStatement.setString(1, bookTitle);
+                ResultSet r1 = prepStatement.executeQuery();
+                if(r1.next()) {
+                    b = new Book();
+                    b.setBookID(r1.getInt("bookID"));
+                    b.setBookTitle(r1.getString("bookTitle"));
+                    b.setBookAuthor(r1.getString("bookAuthor"));
+                    saveBookToCollection(u, col, b);
+                    col.addToBookArray(b);
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+            System.out.println("book added: " + b.getBookTitle());
+            System.out.println("Add new book : ");
+            System.out.println("");
+            if(bookTitle.equals("1")){
+                break;
+            }
+        }
+        System.out.println("Books added to collection");
+        userMenu(u);
+    }
+    public void saveBookToCollection(User u, Collection col, Book b) throws SQLException {
+        PreparedStatement prepStatement = con.prepareStatement("insert into interactions (userID,colID,bookID)" + "values (?,?,?)");
+        prepStatement.setInt(1,u.getID());
+        prepStatement.setInt(2, col.getCollectionID());
+        prepStatement.setInt(3, b.getBookID());
+        prepStatement.executeUpdate();
+    }
+    public void settingsMenu(User u) throws SQLException, IOException, ClassNotFoundException {
         System.out.println("Settings:");
         System.out.println("2. Change Username");
         System.out.println("2. Change Email");
@@ -169,72 +230,79 @@ public class LibraryApp {
         }
     }
 
-    public void LogoutUser(User u) throws SQLException {
-        u = null;
+    public void LogoutUser() throws SQLException {
         loggedIn = false;
         System.out.println("You are now logged out");
     }
 
-    public void changeUsername(User u) throws SQLException {
+    public void changeUsername(User u) throws SQLException, IOException, ClassNotFoundException {
         System.out.println("Choose a new username: ");
         String newUsername = in.next();
-        int updateId = u.getId();
-        PreparedStatement prepStatement = con.prepareStatement("update userAccounts set username=? where userId=?");
+        int updateID = u.getID();
+        PreparedStatement prepStatement = con.prepareStatement("update userAccounts set username=? where userID=?");
         prepStatement.setString(1,newUsername);
-        prepStatement.setInt(2, updateId);
+        prepStatement.setInt(2, updateID);
         prepStatement.executeUpdate();
         System.out.println("Username changed!  ");
         userMenu(u);
     }
-    private void changeEmail(User u) throws SQLException {
+    private void changeEmail(User u) throws SQLException, IOException, ClassNotFoundException {
         System.out.println("Choose a new email: ");
         String newEmail = in.next();
-        int updateId = u.getId();
-        PreparedStatement prepStatement = con.prepareStatement("update userAccounts set email=? where userId=?");
+        int updateID = u.getID();
+        PreparedStatement prepStatement = con.prepareStatement("update userAccounts set email=? where userID=?");
         prepStatement.setString(1,newEmail);
-        prepStatement.setInt(2, updateId);
+        prepStatement.setInt(2, updateID);
         prepStatement.executeUpdate();
         System.out.println("Email changed!  ");
         userMenu(u);
     }
-    public void createCollection(User u) throws SQLException {
-        Collection col = new Collection();
-        col.createCollection();
-        col.showCollectionInfo();
-        userCollections.add(col);
-        serializeCollection(u,col);
-
+    public Collection retrieveCollections(User u) throws SQLException, IOException, ClassNotFoundException {
+        int userID = u.getID();
+        Collection col = null;
+        PreparedStatement prepStatement = con.prepareStatement("SELECT * FROM collections where userID=?");
+        prepStatement.setInt(1, userID);
+        ResultSet r2 = prepStatement.executeQuery();
+        while(r2.next()){
+            byte[] collIn = (byte[]) r2.getObject(1);
+            ByteArrayInputStream byteInColl = new ByteArrayInputStream(collIn);
+            ObjectInputStream objInColl = new ObjectInputStream(byteInColl);
+            col = (Collection) objInColl.readObject();
+            System.out.println("Collection: " + col);
+        }
+        return col;
     }
-    public void serializeCollection(User u, Collection col){
+    /*public void serializeCollectionArray(User u, Collection col){
         byte[] data = null;
-        int userId = u.getId();
+        int userID = u.getID();
         String collName = col.getName();
         String collDesc = col.getDesc();
         LocalDate collCreatedDate = LocalDate.now();
         try{
             ByteArrayOutputStream byteColl = new ByteArrayOutputStream();
             ObjectOutputStream objColl = new ObjectOutputStream(byteColl);
+            objColl.writeObject(col.getBookArray());
+            data = byteColl.toByteArray();
             objColl.flush();
             objColl.close();
             byteColl.close();
-            data = byteColl.toByteArray();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         try{
-            PreparedStatement prepStatement = con.prepareStatement("insert into bookcollections (userId,collectionName,collectionDesc,collectionCreatedDate,collectionSerialize)" + "values (?,?,?,?,?)");
-            prepStatement.setInt(1, userId);
+            PreparedStatement prepStatement = con.prepareStatement("insert into bookcollections (userID,collectionName,collectionDesc,collectionCreatedDate,collectionSerialize)" + "values (?,?,?,?,?)");
+            prepStatement.setInt(1, userID);
             prepStatement.setString(2, collName);
             prepStatement.setString(3,collDesc);
             prepStatement.setDate(4, Date.valueOf(collCreatedDate));
-            prepStatement.setObject(5, data);
+            prepStatement.setBytes(5, data);
             prepStatement.executeUpdate();
-            System.out.println("THis is running");
 
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-    }
+    }*/
     public ResultSet makeQuery(String givenStatement) throws SQLException {
         setSqlStatement(givenStatement);
         return sqlStatement.executeQuery(givenStatement);
